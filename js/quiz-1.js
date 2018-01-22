13756bc6824de7e18873b304d67faa35fbaff9f3
sjmontoyam <sjmontoyam@users.noreply.github.com>
2018-01-22 12:26:20 -0500

    quiz1

---
+++ js/quiz-1.js
@@ -0,0 +1,23 @@
+
+function section1A(){
+	
+	/*var answer = [];
+	answer.push(document.getElementById("respuesta1"));
+	answer.push(document.getElementById("respuesta2"));
+	answer.push(document.getElementById("respuesta3"));
+	answer.push(document.getElementById("respuesta4"));
+	answer.push(document.getElementById("respuesta5"));
+	answer.push(document.getElementById("respuesta6"));
+	answer.push(document.getElementById("respuesta7"));
+	answer.push(document.getElementById("respuesta8"));
+	answer.push(document.getElementById("respuesta9"));*/
+
+	console.log("Hola aqui estoy");
+
+	var printScore = document.getElementById("scoreB");
+	printScore.innerText = "/6";
+
+	alert("Hey ALINA, I love you <3");
+	
+	
+}
